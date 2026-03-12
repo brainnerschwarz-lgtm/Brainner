21dd2fb2194c3af4215281d9a0c29e2fbbe70e1a
Brainner <brainnerschwarz@gmail.com>
2026-03-12 15:06:14 +0100

    Refactor PDF search functionality and document display

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-function searchPDF(){
-
-let input = document.getElementById("searchInput").value.toLowerCase()
-
-let cards = document.querySelectorAll(".card")
-
-cards.forEach(function(card){
-
-let text = card.innerText.toLowerCase()
-
-if(text.includes(input)){
-card.style.display="inline-block"
+const documents = [
+{
+title: "Impact de l'IA",
+type: "Article",
+file: "pdf/ia.pdf"
+},
+{
+title: "Changement climatique",
+type: "Rapport",
+file: "pdf/climat.pdf"
+},
+{
+title: "Gestion de l'eau",
+type: "Thèse",
+file: "pdf/eau.pdf"
 }
-else{
-card.style.display="none"
-}
+]
+
+function afficherDocuments(liste){
+
+let container = document.querySelector(".documents")
+
+container.innerHTML = "<h2>Derniers Documents</h2>"
+
+liste.forEach(doc => {
+
+container.innerHTML += `
+<div class="card">
+<h3>${doc.title}</h3>
+<p>${doc.type}</p>
+<button onclick="ouvrirPDF('${doc.file}')">
+Télécharger PDF
+</button>
+</div>
+`
 
 })
 
 }
+
+function ouvrirPDF(fichier){
+
+window.open(fichier, "_blank")
+
+}
+
+function rechercher(){
+
+let input = document
+.getElementById("searchInput")
+.value
+.toLowerCase()
+
+let resultat = documents.filter(doc =>
+doc.title.toLowerCase().includes(input)
+)
+
+afficherDocuments(resultat)
+
+}
+
+afficherDocuments(documents)
